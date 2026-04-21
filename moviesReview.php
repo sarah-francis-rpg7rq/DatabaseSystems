156adbfb6979d7b5ccac1b3a5082333390a40ace
Sierra <znv2ef@virginia.edu>
2026-04-20 22:58:00 -0400

    Started movies review page

---
+++ moviesReview.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+
+<?php
+require("connect-db.php");   
+require("netflix-db.php");   
+
+//https://www.geeksforgeeks.org/php/how-to-pass-form-variables-from-one-page-to-other-page-in-php/
+
+// Initialize the session
+//session_start();
+     
+// Store the submitted data sent
+// via POST method, stored 
+
+// Temporarily in $_POST structure.
+//get user from previous page
+//$_SESSION['user'] = $_POST['user_name'];
+
+//get movie selected from previous page
+//$_SESSION['movie_selected']
+      //  = $_POST['movie_selected'];
+
+
+
+
+//testing 6 for now but it should get the MID from the previous page
+$list_of_reviews = getReviewsbyMID(6);
+
+?>
+
+
+
+
+<form method="POST" action="">
+     
+    <pre>
+        User(optional): 
+        <input type="text" name="user_to_search">
+    </pre>
+    
+
+    <pre>
+        See: 
+        <input type="radio" name="byUser"
+                value="Show Reviews by User">Show Reviews by User
+        
+        <input type="radio" name="allReviews"
+                value="See All Reviews">See All Reviews
+    </pre>
+     
+
+    <input type="submit" value="Show Reviews">
+ 
+</form>
+
+
+<!-- from POTD code for list of visitors table -->
+<hr/>
+<div class="container">
+<h3>Reviews for (Movie Name would go here)</h3>
+<div class="row justify-content-center">
+<table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
+  <thead>
+  <tr style="background-color:#B0B0B0">
+    <th><b>user</b></th>
+    <th><b>Rating</b></th>
+    <th><b>Review</b></th>
+  </tr>
+  </thead>
+
+  <!-- iterate array of results, display the existing visitor information -->
+  <?php foreach ($list_of_reviews as $row): ?>
+  <tr>
+     <td><?php echo $row['username']; ?> </td>
+     <td><?php echo $row['rating']; ?></td>
+     <td><?php echo $row['review_text']; ?></td>
+
+     
+  </tr>
+  <?php endforeach; ?>
+  <!-- end loop -->
+
+</table>
+</div>
+
+
+</body>
+</html>
+
---
+++ netflix-db.php
@@ -0,0 +1,28 @@
+<?php
+
+
+function getReviewsbyMID($MID)
+{
+    global $db;
+
+    //from milestone code
+    $query = "SELECT r.RID, u.username, m.title, r.rating, r.review_text 
+    FROM review r
+    JOIN writesReview wr ON r.RID = wr.RID
+    JOIN users u ON wr.UID = u.UID
+    JOIN movieHasReview mhr ON r.RID = mhr.RID
+    JOIN movie m ON mhr.MID = m.MID
+    WHERE m.MID=:MID"; //added this to search by a specific movie 
+
+
+    
+    $statement = $db->prepare($query);    
+    $statement->bindValue(':MID', $MID);
+    $statement->execute();              
+    $result = $statement->fetchAll();     
+    $statement->closeCursor();
+    return $result;
+
+}
+
+?>
